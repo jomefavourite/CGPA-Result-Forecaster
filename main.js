d7e7f2e8f614871fbcc3eede1e4680fbc4543087
jomefavourite <jfjomefavourite@gmail.com>
2020-12-29 12:49:34 +0100

    removed dropdown
    Signed-off-by: jomefavourite <jfjomefavourite@gmail.com>

--- main.js
+++ main.js
@@ -16,66 +16,34 @@ const calculate = call("#calculate");
 
 addInput.addEventListener("click", addNewInput);
 calculate.addEventListener("click", () => {
-  const creditUnits = callAll("[name=creditUnit]");
-
-  const selects = [];
-
-  creditUnits.forEach(creditUnit => {
-    return selects.push(creditUnit);
-  });
-
-  console.log(selects);
-  console.log(selects[0][0]);
+  const creditUnits = callAll("#creditUnit");
 
   console.log(creditUnits);
 
-  // let option1 = "";
-  // let option2 = "";
-  // let a = [];
-  // for (let i = 0; i < creditUnit.length; ++i) {
-  //   option1 = creditUnit[i].options[creditUnit[i].selectedIndex].text;
-  //   let totalUnit = document.querySelector("[name = totalUnit]").value;
-  //   totalUnit = Number(totalUnit) + Number(option1);
-  //   a.push(totalUnit);
-  // }
-  // let asum = a.reduce((partial_sum, a) => partial_sum + a, 0);
-
-  // let creditUnitValue = "";
-  // let arrCU = [];
-  // creditUnits.forEach((creditUnit, i) => {
-  //   creditUnitValue = creditUnit.options.innerHTML;
-
-  //   // console.log(creditUnitValue);
-  //   let totalUnit = Number(creditUnitValue);
-
-  //   // console.log(totalUnit);
-  // });
+  function getSelectedOption(sel) {
+    var opt;
+    for (var i = 0, len = sel.options.length; i < len; i++) {
+      opt = sel.options[i];
+      if (opt.selected === true) {
+        break;
+      }
+    }
+    return opt;
+  }
 });
 
 function addNewInput() {
   const tbody = call("#tbody");
   tbody.innerHTML += `
     <tr>
-      <td><input type="text" name="courseCode" id="courseCode" /></td>
       <td>
-        <select name="creditUnit">
-          <option value="6">6</option>
-          <option value="5">5</option>
-          <option value="4">4</option>
-          <option value="3">3</option>
-          <option value="2">2</option>
-          <option value="1">1</option>
-          <option value="0" selected>0</option>
-        </select>
+        <input type="text" name="courseCode" id="courseCode" />
       </td>
       <td>
-        <select name="grade">
-          <option value="A">A</option>
-          <option value="B">B</option>
-          <option value="C">C</option>
-          <option value="D">D</option>
-          <option value="F">F</option>
-        </select>
+        <input type="text" name="creditUnit" id="creditUnit" />
+      </td>
+      <td>
+        <input type="text" name="grade" id="grade" />
       </td>
     </tr>
   `;
