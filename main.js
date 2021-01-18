6ea1968f8a9101685153131ad4d43864ea9f53b2
jomefavourite <jfjomefavourite@gmail.com>
2021-01-18 02:14:45 +0100

    uses select drop down
    Signed-off-by: jomefavourite <jfjomefavourite@gmail.com>

--- main.js
+++ main.js
@@ -169,36 +169,29 @@ function addNewInput() {
           <input type="text" name="courseCode" class="courseCode" />
         </td>
         <td>
-          <input
-            type="num"
-            name="creditUnit"
-            list="creditUnit"
-            class="creditUnit"
-          />
-          <datalist id="creditUnit">
-            <option value="0"></option>
-            <option value="1"></option>
-            <option value="2"></option>
-            <option value="3"></option>
-            <option value="4"></option>
-            <option value="5"></option>
-            <option value="6"></option>
-            <option value="7"></option>
-            <option value="8"></option>
-            <option value="9"></option>
-            <option value="10"></option>
-          </datalist>
+          <select class="creditUnit">
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
         </td>
         <td>
-          <input type="text" name="grade" list="grades" class="grade" />
-            <datalist id="grades">
-              <option value="A"></option>
-              <option value="B"></option>
-              <option value="C"></option>
-              <option value="D"></option>
-              <option value="E"></option>
-              <option value="F"></option>
-            /datalist>
+          <select class="grade">
+            <option value="A">A</option>
+            <option value="B">B</option>
+            <option value="C">C</option>
+            <option value="D">D</option>
+            <option value="E">E</option>
+            <option value="F">F</option>
+          </select>
         </td>
       </tr>
     `;
