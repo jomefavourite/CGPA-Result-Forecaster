18657a7524d78178d6990c3e4ef72bfce8138467
jomefavourite <jfjomefavourite@gmail.com>
2021-01-23 21:40:14 +0100

    update
    Signed-off-by: jomefavourite <jfjomefavourite@gmail.com>

--- serviceworker.js
+++ serviceworker.js
@@ -2,39 +2,37 @@ const staticApp = "CGPA-calculator-v1";
 
 //cache the assets that will be needed offline
 const assets = [
-    "/",
-    "/index.html",
-    "/css/style.css",
-    "/sass/style.css",
-    "/sass/style.css.map",
-    "/sass/style.scss",
-    "/main.js",
-    "images/calculator-logo-72.png",
-    "images/calculator-logo-96.png",
-    "images/calculator-logo-124.png",
-    "images/calculator-logo-144.png",
-    "images/calculator-logo-152.png",
-    "images/calculator-logo-512.png",
-    "images/calculator-logo.png",
-]
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/css/style.css.map",
+  "/sass/style.css",
+  "/sass/style.css.map",
+  "/sass/style.scss",
+  "/main.js",
+  "images/calculator-logo-72.png",
+  "images/calculator-logo-96.png",
+  "images/calculator-logo-124.png",
+  "images/calculator-logo-144.png",
+  "images/calculator-logo-152.png",
+  "images/calculator-logo-512.png",
+  "images/calculator-logo.png",
+];
 
 /*Attach an event listener to the service worker processor(self) to listen to the install event */
 self.addEventListener("install", installEvent => {
-
-    installEvent.waitUntil(
-        caches.open(staticApp).then(
-            cache => {
-                cache.addAll(assets)
-            }
-        )
-    )
+  installEvent.waitUntil(
+    caches.open(staticApp).then(cache => {
+      cache.addAll(assets);
+    })
+  );
 });
 
 //fetch cache
 self.addEventListener("fetch", fetchCache => {
-    fetchCache.respondWith(
-        caches.match(fetchCache.request).then(response => {
-            return response || fetch(fetchCache.request)
-        })
-    )
-})
+  fetchCache.respondWith(
+    caches.match(fetchCache.request).then(response => {
+      return response || fetch(fetchCache.request);
+    })
+  );
+});
